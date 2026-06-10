7413e8ebcaf0b0a28f81b8d668baa3e435a3a40e
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-06-10 14:59:19 +0530

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class myclass
+    {
+        internal void display<T>(T s)
+        {
+            Console.WriteLine(s);
+        }
+        internal void add<T>(T a,T b)
+        {
+            Console.WriteLine("value of a is : " + a);
+            Console.WriteLine("value of b is : " + b);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            myclass obj = new myclass();
+            obj.display<int>(88);
+            obj.display<string>("welcome");
+            obj.display<double>(3.14);
+            obj.display<bool>(true);
+            obj.add<int>(45, 36);
+            obj.add<string>("good", "morning");
+            obj.add<double>(3.45, 6.37);
+            obj.add<bool>(true, false);
+        }
+    }
+}
