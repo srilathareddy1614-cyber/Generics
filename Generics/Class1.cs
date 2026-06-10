56f1c6c9fc4042beb9ffbddd68108abcb08a0241
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-06-10 15:47:44 +0530

    commit

---
+++ Generics/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class employee
+    {
+        internal void Display<t,k,l>(t a,k b,l c)
+        {
+            Console.WriteLine("EMPLOYEE DETAILS");
+            Console.WriteLine("NAME : " + a);
+            Console.WriteLine("ID : " + b);
+            Console.WriteLine("SALARY : " + c);
+        }
+        internal void show<t, K>(t l,K m)
+        {
+            Console.WriteLine("COMPANY DETAILS");
+            Console.WriteLine("COMPANY NAME : " + l);
+            Console.WriteLine("COMPANY LOCATION : " + m);
+        }
+    }
+    internal class Class1
+    {
+        static void Main()
+        {
+            employee obj = new employee();
+            obj.Display<string,int,double>("john",99,55000);
+            obj.show<string,string>("microsoft","banglore");
+
+        }
+    }
+}
--- Generics/Multipledatatype.cs
+++ Generics/Multipledatatype.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Generics
-{
-    class newclass
-    {
-        internal void show<t,k>(t a, k b)
-        {
-            Console.WriteLine("value of a is : " + a);
-            Console.WriteLine("value of b is : " + b);
-        }
-        internal void display<t,k>(t a, k b)
-        {
-            Console.WriteLine("value of a is : " + a);
-            Console.WriteLine("value of b is : " + b);
-        }
-    }
-    internal class Multipledatatype
-    {
-        static void Main()
-        {
-            newclass n = new newclass();
-            n.show<int, string>(10, "hi");
-            n.show<double, bool>(5.6, true);
-            n.display<string, char>("bye", 'z');
-            n.display<float, long>(6.545f, 987654321765);
-        }
-    }
-}
+//namespace Generics
+//{
+//    class newclass
+//    {
+//        internal void show<t,k>(t a, k b)
+//        {
+//            Console.WriteLine("value of a is : " + a);
+//            Console.WriteLine("value of b is : " + b);
+//        }
+//        internal void display<t,k>(t a, k b)
+//        {
+//            Console.WriteLine("value of a is : " + a);
+//            Console.WriteLine("value of b is : " + b);
+//        }
+//    }
+//    internal class Multipledatatype
+//    {
+//        static void Main()
+//        {
+//            newclass n = new newclass();
+//            n.show<int, string>(10, "hi");
+//            n.show<double, bool>(5.6, true);
+//            n.display<string, char>("bye", 'z');
+//            n.display<float, long>(6.545f, 987654321765);
+//        }
+//    }
+//}
