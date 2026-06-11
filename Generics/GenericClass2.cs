4571282ca1b86b03699829a1d5d5d213289f0ab0
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-06-11 10:46:36 +0530

    commit

---
+++ Generics/GenericClass2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class newclass<k,l,m>
+    {
+        k a;
+        l b;
+        m c;
+        internal newclass(k a, l b, m c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void display()
+        {
+            Console.WriteLine("employee id : " + a);
+            Console.WriteLine("employee name : " + b);
+            Console.WriteLine("employee salary : " + c);
+            Console.WriteLine("`````````````````````````````````````");
+        }
+    }
+    internal class GenericClass2
+    {
+        static void Main()
+        {
+            newclass<int, string, double> m =new newclass<int, string, double>(102, "ravi", 55000.55);
+            m.display();
+            newclass<int, string, double> n = new newclass<int, string, double>(105, "john", 46000.00);
+            n.display();
+        }
+    }
+}
--- Generics/Genericclass.cs
+++ Generics/Genericclass.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Generics
-{
-    class myclass<t>
-    {
-        t a;
-        internal myclass(t a)
-        {
-            this.a = a;
-        }
-        public void display()
-        {
-            Console.WriteLine("value of a : " + a);
-        }
-    }
-    internal class Genericclass
-    {
-        static void Main()
-        {
-            myclass<int> m = new myclass<int>(105);
-            m.display();
-            myclass<string> mc = new myclass<string>("good morning");
-            mc.display();
-        }
-    }
-}
+//namespace Generics
+//{
+//    class myclass<t>
+//    {
+//        t a;
+//        internal myclass(t a)
+//        {
+//            this.a = a;
+//        }
+//        public void display()
+//        {
+//            Console.WriteLine("value of a : " + a);
+//        }
+//    }
+//    internal class Genericclass
+//    {
+//        static void Main()
+//        {
+//            myclass<int> m = new myclass<int>(105);
+//            m.display();
+//            myclass<string> mc = new myclass<string>("good morning");
+//            mc.display();
+//        }
+//    }
+//}
